812475a9a471cc93eea375f6429faaf2250285cd
ManavK003 <manavkm03@gmail.com>
2025-08-18 17:27:05 -0400

    Adding backend url link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import bg7 from './assets/bg7.jpg';
 import bg10 from './assets/bg10.jpg';
 import bg99 from './assets/bg99.png';
 
-const API_URL = 'http://127.0.0.1:5001';
+const API_URL = 'https://research-project-testing-1-production.up.railway.app' ;
 
 const TrashIcon = ({ className }) => (<svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/></svg>);
 const EditIcon = ({ className }) => (<svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>);
